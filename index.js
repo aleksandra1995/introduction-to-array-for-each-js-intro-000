77854609b422c73435046c21adc047981ea4208e
Aleksandra Kosc <aleksandra.kosc@gmail.com>
2019-06-06 19:41:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,6 @@ function doToElementsInArray(array,callback) {
 }
 
 // Add your changeCompletely() function here:
+function changeCompletely(element,index,array) {
+  array[index] =math.floor(math.random()) * 100+2).toString() + `${array[index]}s!!!` }
+}
